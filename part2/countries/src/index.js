d97edfc5c7f6d17e13c1889815b13b83b14165c4
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2022-12-28 17:28:52 +0100

    Create index.js

---
+++ part2/countries/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+
+// set REACT_APP_API_KEY=<OPEN_WEATHER_API_KEY> && npm start
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
